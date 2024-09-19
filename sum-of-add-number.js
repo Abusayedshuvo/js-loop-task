59ace79e4fe25541fc732e10ddaa170c3c2e57ce
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-09-19 17:49:44 +0600

    js for loop sum of add numbers

---
+++ sum-of-add-number.js
@@ -0,0 +1,16 @@
+let sum = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 === 1) {
+    sum = sum + i;
+  }
+}
+console.log(sum);
+
+let sum2 = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    sum2 = sum2 + i;
+  }
+}
+
+console.log(sum2);
