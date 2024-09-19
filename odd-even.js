4140ba031bfd24c671f86b6a2ff24c2376696577
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-09-19 17:32:59 +0600

    js for loop odd-even number

---
+++ odd-even.js
@@ -0,0 +1,11 @@
+for (let num = 61; num < 100; num++) {
+  if (num % 2 !== 0) {
+    console.log(num);
+  }
+}
+
+for (let num = 78; num < 98; num++) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
